a0362684ca2ece248e5ff452ebd9950a6edcf064
hth <hth@users.noreply.github.com>
2017-04-15 21:44:13 +0530

    removed cache

--- domain/src/main/java/com/noqapp/repository/BizStoreManager.java
+++ domain/src/main/java/com/noqapp/repository/BizStoreManager.java
@@ -1,7 +1,5 @@
 package com.noqapp.repository;
 
-import org.springframework.cache.annotation.Cacheable;
-
 import com.noqapp.domain.BizNameEntity;
 import com.noqapp.domain.BizStoreEntity;
 
@@ -95,7 +93,6 @@ public interface BizStoreManager extends RepositoryManager<BizStoreEntity> {
      */
     List<BizStoreEntity> getAllBizStores(String bizNameId);
 
-    @Cacheable (value = "findByCodeQR", keyGenerator = "customKeyGenerator")
     BizStoreEntity findByCodeQR(String codeQR);
 
     boolean isValidCodeQR(String codeQR);
