f73f4432ba5dd15da3fb8624b84c2638e632bf61
hth <hth@users.noreply.github.com>
2017-05-27 08:10:57 +0530

    Added method cmputeCurrrentlyServing

--- domain/src/main/java/com/noqapp/domain/TokenQueueEntity.java
+++ domain/src/main/java/com/noqapp/domain/TokenQueueEntity.java
@@ -130,4 +130,15 @@ public class TokenQueueEntity extends BaseEntity {
             return "no";
         }
     }
+
+    @Transient
+    public int computeCurrentlyServing() {
+        switch (queueStatus) {
+            case C:
+            case D:
+                return 0;
+            default:
+                return currentlyServing;
+        }
+    }
 }
