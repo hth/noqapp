cdefdccad6576aebe3491f688f2d04d75cbfbc32
hth <hth@users.noreply.github.com>
2017-07-26 23:02:11 +0530

    Added OTP

--- view/src/main/java/com/noqapp/view/controller/open/LoginController.java
+++ view/src/main/java/com/noqapp/view/controller/open/LoginController.java
@@ -53,7 +53,7 @@ public class LoginController {
     private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
 
     //@Value ("${loginPage:digits}")
-    @Value ("${loginPage:login}")
+    @Value ("${loginPage:login5}")
     private String loginPage;
 
     //private UserAgentStringParser parser;
