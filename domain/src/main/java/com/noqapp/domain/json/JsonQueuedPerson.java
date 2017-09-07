d1ff05a2d3203ebc753a75d6db77e47a1ad194a1
hth <hth@users.noreply.github.com>
2017-09-07 06:36:42 -0700

    Added new json

---
+++ domain/src/main/java/com/noqapp/domain/json/JsonQueuedPerson.java
@@ -0,0 +1,57 @@
+package com.noqapp.domain.json;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.noqapp.domain.AbstractDomain;
+
+/**
+ * User: hitender
+ * Date: 9/7/17 6:24 AM
+ */
+@SuppressWarnings ({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable",
+        "unused"
+})
+@JsonAutoDetect (
+        fieldVisibility = JsonAutoDetect.Visibility.ANY,
+        getterVisibility = JsonAutoDetect.Visibility.NONE,
+        setterVisibility = JsonAutoDetect.Visibility.NONE
+)
+@JsonPropertyOrder (alphabetic = true)
+@JsonIgnoreProperties (ignoreUnknown = true)
+public class JsonQueuedPerson extends AbstractDomain {
+    private static final Logger LOG = LoggerFactory.getLogger(JsonQueuedPerson.class);
+
+    @JsonProperty ("t")
+    private int token;
+
+    @JsonProperty ("n")
+    private String customerName = "";
+
+    public int getToken() {
+        return token;
+    }
+
+    public JsonQueuedPerson setToken(int token) {
+        this.token = token;
+        return this;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public JsonQueuedPerson setCustomerName(String customerName) {
+        this.customerName = customerName;
+        return this;
+    }
+}
