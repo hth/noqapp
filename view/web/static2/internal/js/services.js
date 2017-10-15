dbf24b227a55a713c428124ce37d7abf99ac07e4
hth <hth@users.noreply.github.com>
2017-10-15 18:37:07 +0530

    added semi-colon

--- view/web/static2/internal/js/services.js
+++ view/web/static2/internal/js/services.js
@@ -4,7 +4,7 @@ function handleFoundAddressClick() {
 
 function handleFoundAddressCheckboxUncheck() {
     $("input[name$=foundAddressPlaceId]").prop('checked', false);
-    $('#addressCheckBox .form-check-box').prop("disabled", true)
+    $('#addressCheckBox .form-check-box').prop("disabled", true);
 }
 
 function handleFoundAddressStoreClick() {
@@ -13,5 +13,6 @@ function handleFoundAddressStoreClick() {
 
 function handleFoundAddressStoreCheckboxUncheck() {
     $("input[name$=foundAddressStorePlaceId]").prop('checked', false);
-    $('#addressStoreCheckBox .form-check-box').prop("disabled", true)
+    $('#addressStoreCheckBox .form-check-box').prop("disabled", true);
+}
 }
