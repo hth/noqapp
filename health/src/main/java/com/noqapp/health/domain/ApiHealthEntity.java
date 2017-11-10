b786a6e39c92780ae67c805587a731e051a0e278
hth <hth@users.noreply.github.com>
2017-11-10 10:37:36 +0530

    changed name of index

--- health/src/main/java/com/noqapp/health/domain/ApiHealthEntity.java
+++ health/src/main/java/com/noqapp/health/domain/ApiHealthEntity.java
@@ -42,7 +42,7 @@ public class ApiHealthEntity extends BaseEntity {
     private HealthStatusEnum healthStatus;
 
     /* Auto delete records in 2 days. */
-    @Indexed(name="someDateFieldIndex", expireAfterSeconds=172800)
+    @Indexed(name="health_service_auto_delete_idx", expireAfterSeconds=172800)
     @Field ("C")
     private Date created = new Date();
 
