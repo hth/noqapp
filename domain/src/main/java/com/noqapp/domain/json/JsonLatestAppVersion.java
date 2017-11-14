3e329ca15800c10432eadc7c756ded9dbde34693
hth <hth@users.noreply.github.com>
2017-11-14 17:06:52 +0530

    renamed file

--- domain/src/main/java/com/noqapp/domain/json/JsonLatestAppVersion.java
+++ domain/src/main/java/com/noqapp/domain/json/JsonLatestAppVersion.java
@@ -24,12 +24,12 @@ import com.noqapp.domain.AbstractDomain;
 )
 @JsonPropertyOrder(alphabetic = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class JsonSupportedDevice extends AbstractDomain {
+public class JsonLatestAppVersion extends AbstractDomain {
 
     @JsonProperty("av")
     private String latestAppVersion;
 
-    public JsonSupportedDevice(String latestAppVersion) {
+    public JsonLatestAppVersion(String latestAppVersion) {
         this.latestAppVersion = latestAppVersion;
     }
 }
