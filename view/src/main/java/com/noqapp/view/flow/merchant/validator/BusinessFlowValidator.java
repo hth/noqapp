be5262b9b6f5cf9142d5efdca9c062526afd216d
hth <hth@users.noreply.github.com>
2017-12-14 10:54:18 +0530

    updated text

--- view/src/main/java/com/noqapp/view/flow/merchant/validator/BusinessFlowValidator.java
+++ view/src/main/java/com/noqapp/view/flow/merchant/validator/BusinessFlowValidator.java
@@ -296,7 +296,7 @@ public class BusinessFlowValidator {
                     new MessageBuilder()
                             .error()
                             .source(source + "storeBusinessType")
-                            .defaultText("'Queue for' has to be a subset of 'Business Type' selected for business " + registerBusiness.getName())
+                            .defaultText("'Queue for' has to be a subset of 'Business Type'")
                             .build());
             status = "failure";
         }
