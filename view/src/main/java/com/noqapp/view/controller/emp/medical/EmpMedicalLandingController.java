ec7fbed0453b777876228e1de4428a7a11b059bb
hth <hth@users.noreply.github.com>
2018-04-17 07:58:48 +0530

    updated log

--- view/src/main/java/com/noqapp/view/controller/emp/medical/EmpMedicalLandingController.java
+++ view/src/main/java/com/noqapp/view/controller/emp/medical/EmpMedicalLandingController.java
@@ -57,7 +57,7 @@ public class EmpMedicalLandingController {
     ) throws IOException {
         QueueUser queueUser = (QueueUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (queueUser.getUserLevel() == UserLevelEnum.MEDICAL_TECHNICIAN) {
-            LOG.warn("Could not find qid={} having access as business user", queueUser.getQueueUserId());
+            LOG.warn("Could not find qid={} having access as medical user", queueUser.getQueueUserId());
             response.sendError(SC_NOT_FOUND, "Could not find");
             return null;
         }
