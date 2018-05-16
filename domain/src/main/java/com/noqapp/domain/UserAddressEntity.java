577b34c3369fd3bc66dae3d93c6ec676a4b24eab
hth <hth@users.noreply.github.com>
2018-05-16 14:56:09 +0530

    added support for multiple user address

---
+++ domain/src/main/java/com/noqapp/domain/UserAddressEntity.java
@@ -0,0 +1,76 @@
+package com.noqapp.domain;
+
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * hitender
+ * 5/15/18 9:23 PM
+ */
+@SuppressWarnings ({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable"
+})
+@Document(collection = "USER_ADDRESS")
+@CompoundIndexes({
+        @CompoundIndex(name = "user_address_idx", def = "{'QID': 1}", unique = false)
+})
+public class UserAddressEntity extends BaseEntity {
+
+    @NotNull
+    @Field("QID")
+    private String queueUserId;
+
+    @Field ("AD")
+    private String address;
+
+    @Field ("CS")
+    private String countryShortName;
+
+    @Field ("LU")
+    private Date lastUsed;
+
+    @SuppressWarnings("unused")
+    private UserAddressEntity() {
+        super();
+    }
+
+    public UserAddressEntity(String queueUserId, String address) {
+        this.queueUserId = queueUserId;
+        this.address = address;
+        this.setLastUsed();
+    }
+
+    public String getQueueUserId() {
+        return queueUserId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCountryShortName() {
+        return countryShortName;
+    }
+
+    public UserAddressEntity setCountryShortName(String countryShortName) {
+        this.countryShortName = countryShortName;
+        return this;
+    }
+
+    public Date getLastUsed() {
+        return lastUsed;
+    }
+
+    public UserAddressEntity setLastUsed() {
+        this.lastUsed = new Date();
+        return this;
+    }
+}
---
+++ domain/src/main/java/com/noqapp/repository/UserAddressManagerImpl.java
@@ -0,0 +1,106 @@
+package com.noqapp.repository;
+
+import com.noqapp.domain.BaseEntity;
+import com.noqapp.domain.UserAddressEntity;
+import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static org.springframework.data.domain.Sort.Direction.ASC;
+import static org.springframework.data.domain.Sort.Direction.DESC;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+/**
+ * hitender
+ * 5/15/18 10:45 PM
+ */
+@SuppressWarnings({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable"
+})
+@Repository
+public class UserAddressManagerImpl implements UserAddressManager {
+    private static final Logger LOG = LoggerFactory.getLogger(UserAddressManagerImpl.class);
+    private static final String TABLE = BaseEntity.getClassAnnotationValue(
+            UserAddressEntity.class,
+            Document.class,
+            "collection");
+
+    private int numberOfAddressAllowed;
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    public UserAddressManagerImpl(
+            @Value("${UserAddressManagerImpl.numberOfAddressAllowed}")
+            int numberOfAddressAllowed,
+
+            MongoTemplate mongoTemplate
+    ) {
+        this.numberOfAddressAllowed = numberOfAddressAllowed;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public void save(UserAddressEntity object) {
+        if (object.getId() != null) {
+            object.setUpdated();
+            mongoTemplate.save(object, TABLE);
+        } else {
+            while (count(object.getQueueUserId()) >= numberOfAddressAllowed) {
+                UserAddressEntity leastUsed = leastUsedAddress(object.getQueueUserId());
+                mongoTemplate.remove(leastUsed);
+            }
+            mongoTemplate.save(object, TABLE);
+        }
+    }
+
+    @Override
+    public void deleteHard(UserAddressEntity object) {
+        mongoTemplate.remove(object);
+    }
+
+    @Override
+    public void deleteAddress(String id, String qid) {
+        mongoTemplate.remove(
+                query(where("id").is(new ObjectId(id)).and("QID").is(qid)),
+                UserAddressEntity.class,
+                TABLE
+        );
+    }
+
+    @Override
+    public List<UserAddressEntity> getAll(String qid) {
+        return mongoTemplate.find(
+                query(where("QID").is(qid)).with(new Sort(ASC, "LU")),
+                UserAddressEntity.class,
+                TABLE
+        );
+    }
+
+    private long count(String qid) {
+        return mongoTemplate.count(
+                query(where("QID").is(qid)),
+                UserAddressEntity.class,
+                TABLE
+        );
+    }
+
+    private UserAddressEntity leastUsedAddress(String qid) {
+        return mongoTemplate.findOne(
+                query(where("QID").is(qid)).with(new Sort(DESC, "LU")),
+                UserAddressEntity.class,
+                TABLE
+        );
+    }
+}
