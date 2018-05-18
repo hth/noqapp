fdddad4d7f902ed113f79e22be1428534f438bfd
hth <hth@users.noreply.github.com>
2018-05-18 16:51:43 +0530

    optimized import

--- domain/src/main/java/com/noqapp/domain/flow/MigrateToBusinessRegistration.java
+++ domain/src/main/java/com/noqapp/domain/flow/MigrateToBusinessRegistration.java
@@ -1,9 +1,9 @@
 package com.noqapp.domain.flow;
 
+import com.noqapp.common.utils.ScrubbedInput;
 import com.noqapp.domain.BizNameEntity;
 import com.noqapp.domain.BizStoreEntity;
 import com.noqapp.domain.BusinessUserEntity;
-import com.noqapp.common.utils.ScrubbedInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
