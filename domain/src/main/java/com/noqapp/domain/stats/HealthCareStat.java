1620450b1bfced24e94ce747da289cbfb8c1ba9c
hth <hth@users.noreply.github.com>
2018-06-06 17:08:55 +0530

    added condition to get stats for all the health care queues

---
+++ domain/src/main/java/com/noqapp/domain/stats/HealthCareStat.java
@@ -0,0 +1,68 @@
+package com.noqapp.domain.stats;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.noqapp.common.utils.AbstractDomain;
+
+import java.util.List;
+
+/**
+ * hitender
+ * 5/15/18 9:47 AM
+ */
+@SuppressWarnings ({
+        "PMD.BeanMembersShouldSerialize",
+        "PMD.LocalVariableCouldBeFinal",
+        "PMD.MethodArgumentCouldBeFinal",
+        "PMD.LongVariable",
+        "unused"
+})
+@JsonAutoDetect(
+        fieldVisibility = JsonAutoDetect.Visibility.ANY,
+        getterVisibility = JsonAutoDetect.Visibility.NONE,
+        setterVisibility = JsonAutoDetect.Visibility.NONE
+)
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class HealthCareStat extends AbstractDomain {
+
+    @JsonProperty("c")
+    private String codeQR;
+
+    @JsonProperty("yearly")
+    private List<YearlyData> twelveMonths;
+
+    @JsonProperty("rc")
+    private NewRepeatCustomers repeatCustomers;
+
+    public String getCodeQR() {
+        return codeQR;
+    }
+
+    public HealthCareStat setCodeQR(String codeQR) {
+        this.codeQR = codeQR;
+        return this;
+    }
+
+    public List<YearlyData> getTwelveMonths() {
+        return twelveMonths;
+    }
+
+    public HealthCareStat setTwelveMonths(List<YearlyData> twelveMonths) {
+        this.twelveMonths = twelveMonths;
+        return this;
+    }
+
+    public NewRepeatCustomers getRepeatCustomers() {
+        return repeatCustomers;
+    }
+
+    public HealthCareStat setRepeatCustomers(NewRepeatCustomers repeatCustomers) {
+        this.repeatCustomers = repeatCustomers;
+        return this;
+    }
+}
