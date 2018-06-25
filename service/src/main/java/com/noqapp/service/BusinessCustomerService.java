3fd0c36af004d169edbad09988124228f915c02c
hth <hth@users.noreply.github.com>
2018-06-25 14:43:21 +0530

    added method to find one by qid

--- service/src/main/java/com/noqapp/service/BusinessCustomerService.java
+++ service/src/main/java/com/noqapp/service/BusinessCustomerService.java
@@ -2,10 +2,12 @@ package com.noqapp.service;
 
 import com.noqapp.domain.BizStoreEntity;
 import com.noqapp.domain.BusinessCustomerEntity;
+import com.noqapp.domain.QueueEntity;
 import com.noqapp.domain.UserProfileEntity;
 import com.noqapp.domain.annotation.Mobile;
 import com.noqapp.repository.BizStoreManager;
 import com.noqapp.repository.BusinessCustomerManager;
+import com.noqapp.repository.QueueManager;
 import com.noqapp.repository.UserProfileManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,16 +22,19 @@ public class BusinessCustomerService {
     private BusinessCustomerManager businessCustomerManager;
     private BizStoreManager bizStoreManager;
     private UserProfileManager userProfileManager;
+    private QueueManager queueManager;
 
     @Autowired
     public BusinessCustomerService(
             BusinessCustomerManager businessCustomerManager,
             BizStoreManager bizStoreManager,
-            UserProfileManager userProfileManager
+            UserProfileManager userProfileManager,
+            QueueManager queueManager
     ) {
         this.businessCustomerManager = businessCustomerManager;
         this.bizStoreManager = bizStoreManager;
         this.userProfileManager = userProfileManager;
+        this.queueManager = queueManager;
     }
 
     /**
@@ -38,11 +43,17 @@ public class BusinessCustomerService {
     @Mobile
     public void addBusinessCustomer(String qid, String codeQR, String businessCustomerId) {
         BizStoreEntity bizStore = bizStoreManager.findByCodeQR(codeQR);
-        businessCustomerManager.save(new BusinessCustomerEntity(
+        BusinessCustomerEntity businessCustomer = new BusinessCustomerEntity(
                 qid,
                 bizStore.getBizName().getId(),
                 businessCustomerId
-        ));
+        );
+        businessCustomerManager.save(businessCustomer);
+
+        /* Update queue with business customer id. */
+        QueueEntity queue = queueManager.findOneQueueByQid(qid, codeQR);
+        queue.setBusinessCustomerId(businessCustomerId);
+        queueManager.save(queue);
     }
 
     @Mobile
