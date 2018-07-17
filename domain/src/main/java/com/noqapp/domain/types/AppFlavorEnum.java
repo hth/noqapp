4b42ade3422d91c5c3000bddb97414bbe8bc5f7c
hth <hth@users.noreply.github.com>
2018-07-17 14:56:47 +0530

    Added app flavor to distinguish different app

---
+++ domain/src/main/java/com/noqapp/domain/types/AppFlavorEnum.java
@@ -0,0 +1,38 @@
+package com.noqapp.domain.types;
+
+import com.noqapp.domain.annotation.Mobile;
+
+/**
+ * App flavor to distinguish between different apps.
+ * hitender
+ * 7/17/18 11:41 AM
+ */
+@Mobile
+public enum AppFlavorEnum {
+    NQCL("NQCL", "NoQueue Client"),
+    NQCH("NQCH", "NoQueue Client Hospital"),
+    NQMS("NQMS", "NoQueue Merchant Store"),
+    NQMH("NQMH", "NoQueue Merchant HealthCare"),
+    NQMT("NQMT", "NoQueue Merchant TV");
+
+    private String name;
+    private String description;
+
+    AppFlavorEnum(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
