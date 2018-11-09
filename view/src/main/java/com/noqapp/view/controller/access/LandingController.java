842445be73237e030c363172478d55eccd472de1
hth <hth@users.noreply.github.com>
2018-11-09 22:16:04 +0530

    added exception

--- view/src/main/java/com/noqapp/view/controller/access/LandingController.java
+++ view/src/main/java/com/noqapp/view/controller/access/LandingController.java
@@ -79,33 +79,39 @@ public class LandingController {
             @ModelAttribute("landingForm")
             LandingForm landingForm
     ) {
-        Instant start = Instant.now();
-        LOG.info("Landed on next page");
-        QueueUser queueUser = (QueueUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        BusinessUserEntity businessUser = businessUserService.loadBusinessUser();
-        if (null != businessUser) {
-            landingForm.setBusinessUserRegistrationStatus(businessUser.getBusinessUserRegistrationStatus())
+        try {
+            Instant start = Instant.now();
+            LOG.info("Landed on next page");
+            QueueUser queueUser = (QueueUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            BusinessUserEntity businessUser = businessUserService.loadBusinessUser();
+            if (null != businessUser) {
+                landingForm
+                    .setBusinessUserRegistrationStatus(businessUser.getBusinessUserRegistrationStatus())
                     .setBusinessAccountSignedUp(businessUser.getUpdated());
-        }
+            }
 
-        if (queueUser.getUserLevel() != UserLevelEnum.M_ADMIN) {
-            landingForm
+            if (UserLevelEnum.M_ADMIN != queueUser.getUserLevel()) {
+                landingForm
                     .setCurrentQueues(queueService.findAllQueuedByQid(queueUser.getQueueUserId()))
                     .setHistoricalQueues(queueService.findAllHistoricalQueue(queueUser.getQueueUserId()))
                     .setMinorUserProfiles(accountService.findDependentProfiles(queueUser.getQueueUserId()));
 
-            LOG.info("Current size={} and Historical size={}",
+                LOG.info("Current size={} and Historical size={}",
                     landingForm.getCurrentQueues().size(),
                     landingForm.getHistoricalQueues().size());
-        }
+            }
 
-        apiHealthService.insert(
+            apiHealthService.insert(
                 "/landing",
                 "landing",
                 LandingController.class.getName(),
                 Duration.between(start, Instant.now()),
                 HealthStatusEnum.G);
-        return nextPage;
+            return nextPage;
+        } catch (Exception e) {
+            LOG.error("Failed loading page reason={}", e.getLocalizedMessage(), e);
+            throw e;
+        }
     }
 
     @GetMapping(value = "/landing/business/migrate")
