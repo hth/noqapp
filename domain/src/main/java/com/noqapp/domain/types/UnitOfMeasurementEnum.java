1bb09b2d507a6ff3961face6daeed6aaec49933c
hth <hth@users.noreply.github.com>
2018-11-13 20:40:22 +0530

    added unit of measurement for pharmacy

--- domain/src/main/java/com/noqapp/domain/types/UnitOfMeasurementEnum.java
+++ domain/src/main/java/com/noqapp/domain/types/UnitOfMeasurementEnum.java
@@ -1,5 +1,6 @@
 package com.noqapp.domain.types;
 
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,11 +11,24 @@ import java.util.Map;
 public enum UnitOfMeasurementEnum {
     CN("CN", "count (nos)"),
     DZ("DZ", "dozen"),
-    MG("MG", "mg (milligram)"),
-    GM("GM", "gm (gram)"),
-    KG("KG", "kg (kilogram)"),
-    ML("ML", "ml (milliliter)"),
-    LT("LT", "lt (liter)");
+    HD("1/2 DZ", "1/2 dozen"),
+    MG("MG", "mg"),
+    GM("GM", "gm"),
+    KG("KG", "kg"),
+    ML("ML", "ml"),
+    LT("LT", "lt"),
+
+    CM("CM", "cm"),
+    LA("LA", "Large"),
+    XL("XL", "Extra Large"),
+    ME("ME", "Medium"),
+    SM("SM", "Small"),
+    KT("KT", "Kit");
+
+    public static EnumSet<UnitOfMeasurementEnum> PHARMACY = EnumSet.of(MG, GM, ML, LT, CM, LA, XL, ME, SM, KT);
+    public static UnitOfMeasurementEnum[] PHARMACY_VALUES = {MG, GM, ML, LT, CM, LA, XL, ME, SM, KT};
+    public static EnumSet<UnitOfMeasurementEnum> GROCERY = EnumSet.of(CN, HD, DZ, MG, GM, KG, ML, LT);
+    public static UnitOfMeasurementEnum[] GROCERY_VALUES = {CN, HD, DZ, MG, GM, KG, ML, LT};
 
     private final String name;
     private final String description;
@@ -35,12 +49,20 @@ public enum UnitOfMeasurementEnum {
     public static Map<String, String> getAsMap() {
         return new HashMap<String, String>() {{
             put(CN.description, CN.name);
+            put(HD.description, HD.name);
             put(DZ.description, DZ.name);
             put(MG.description, MG.name);
             put(GM.description, GM.name);
             put(KG.description, KG.name);
             put(ML.description, ML.name);
             put(LT.description, LT.name);
+
+            put(CM.description, CM.name);
+            put(LA.description, LA.name);
+            put(XL.description, XL.name);
+            put(ME.description, ME.name);
+            put(SM.description, SM.name);
+            put(KT.description, KT.name);
         }};
     }
 
