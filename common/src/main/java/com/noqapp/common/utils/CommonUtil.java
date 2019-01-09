27d58473fc2ade139f52c696d0605a22503013e5
hth <hth@users.noreply.github.com>
2019-01-09 14:04:47 +0530

    added log

--- common/src/main/java/com/noqapp/common/utils/CommonUtil.java
+++ common/src/main/java/com/noqapp/common/utils/CommonUtil.java
@@ -184,10 +184,13 @@ public final class CommonUtil {
     }
 
     public static String generateTransactionId(String storeId, int token) {
-        return storeId.substring(18, 24)
+        String transactionId = storeId.substring(18, 24)
             + "-" + token
             + "-" + (random.ints(0, 100).findFirst().getAsInt() + 100)
             + "-" + generateHexFromObjectId().substring(0, 8);
+        
+        LOG.info("{}", transactionId);
+        return transactionId;
     }
 
     /**
