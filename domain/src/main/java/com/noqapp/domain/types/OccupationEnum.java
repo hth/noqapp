85d2b41e2e0e3aab45ce653d53ca534948de0dc4
hth <hth@users.noreply.github.com>
2019-05-13 11:58:29 +0530

    Updated missing enums

--- domain/src/main/java/com/noqapp/domain/types/OccupationEnum.java
+++ domain/src/main/java/com/noqapp/domain/types/OccupationEnum.java
@@ -5,13 +5,18 @@ package com.noqapp.domain.types;
  * 7/30/18 3:37 PM
  */
 public enum OccupationEnum {
-    CMP("CPR", "Computer Professional"),
+    CMP("CMP", "Computer Professional"),
     DOC("DOC", "Doctor"),
     ENG("ENG", "Engineer"),
     STU("STU", "Student"),
     TEC("TEC", "Teacher"),
     POL("POL", "Police"),
-    LAW("LAW", "Lawyer");
+    HOW("HOW", "House Wife"),
+    RET("RET", "Retired"),
+    BAN("BAN", "Banker"),
+    SER("SER", "Service Industry"),
+    LAW("LAW", "Lawyer"),
+    GOV("GOV", "Government Official");
 
     private final String name;
     private final String description;
