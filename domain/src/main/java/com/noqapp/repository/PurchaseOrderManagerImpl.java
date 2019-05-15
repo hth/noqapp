ddb51e07bf081d75d65b04f4114f48acd5d064db
hth <hth@users.noreply.github.com>
2019-05-15 23:41:18 +0530

    Cancel order when returned back from gateway without payment

--- domain/src/main/java/com/noqapp/repository/PurchaseOrderManagerImpl.java
+++ domain/src/main/java/com/noqapp/repository/PurchaseOrderManagerImpl.java
@@ -579,6 +579,21 @@ public class PurchaseOrderManagerImpl implements PurchaseOrderManager {
     }
 
     @Override
+    public void cancelOrderWhenBackedAwayFromGateway(String transactionId) {
+        mongoTemplate.updateFirst(
+            query(where("TI").is(transactionId)
+                .orOperator(
+                    where("DM").is(DeliveryModeEnum.QS), //TEMP Line
+                    where("PS").is(PurchaseOrderStateEnum.VB)
+                )),
+            update("PS", PurchaseOrderStateEnum.CO).push("OS", PurchaseOrderStateEnum.CO)
+                .set("PY", PaymentStatusEnum.PC),
+            PurchaseOrderEntity.class,
+            TABLE
+        );
+    }
+
+    @Override
     public List<PurchaseOrderEntity> findByBizNameId(String bizNameId) {
         return mongoTemplate.find(
             query(where("BN").is(bizNameId)).with(new Sort(DESC, "C")),
