4e0f4f07daca79ff9fbc33d6bbd359b542d21174
hth <hth@users.noreply.github.com>
2020-02-11 23:58:07 -0800

    Fixed missing variable

--- view/src/main/java/com/noqapp/view/controller/open/WebJoinQueueController.java
+++ view/src/main/java/com/noqapp/view/controller/open/WebJoinQueueController.java
@@ -190,9 +190,7 @@ public class WebJoinQueueController {
                 }
             }
 
-            if (bizStore.getBizName().isNotClaimed()) {
-                rootMap.put("claimed", bizStore.getBizName().isClaimed() ? "Yes" : "No");
-            }
+            rootMap.put("claimed", bizStore.getBizName().isClaimed() ? "Yes" : "No");
             webJoinQueue.setRootMap(rootMap).setCodeQR(new ScrubbedInput(((String) rootMap.get("codeQR"))));
 
             model.addAttribute("firebaseApiKey", firebaseApiKey);
@@ -252,10 +250,7 @@ public class WebJoinQueueController {
                     }
                     rootMap.put("expectedServiceTime", expectedServiceTime);
                     rootMap.put("storeAddress", bizStore.getAddress());
-
-                    if (bizStore.getBizName().isNotClaimed()) {
-                        rootMap.put("claimed", bizStore.getBizName().isClaimed() ? "Yes" : "No");
-                    }
+                    rootMap.put("claimed", bizStore.getBizName().isClaimed() ? "Yes" : "No");
                     webJoinQueue.setRootMap(rootMap).setCodeQR(new ScrubbedInput(bizStore.getCodeQR()));
                     return joinQueueConfirmPage;
                 }
