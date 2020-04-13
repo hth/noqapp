94749b600cd6d2f83840bb9306012e5d35c19aa3
hth <hth@users.noreply.github.com>
2020-04-12 20:31:48 -0700

    Updated header

--- portal/src/main/java/com/noqapp/portal/medical/MedicalDashboard.java
+++ portal/src/main/java/com/noqapp/portal/medical/MedicalDashboard.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -46,7 +47,7 @@ public class MedicalDashboard {
     }
 
     @GetMapping(
-        produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"
+        produces = MediaType.APPLICATION_JSON_VALUE
     )
     public String populateDashBoard(
         @RequestHeader("X-R-DID")
