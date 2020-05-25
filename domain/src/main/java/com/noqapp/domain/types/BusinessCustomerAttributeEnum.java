ca09cbae3a9f4f332d33224e689de457e3bcc358
hth <hth@users.noreply.github.com>
2020-05-25 05:07:05 -0700

    Added customer attribute and priority level

---
+++ domain/src/main/java/com/noqapp/domain/types/BusinessCustomerAttributeEnum.java
@@ -0,0 +1,33 @@
+package com.noqapp.domain.types;
+
+/**
+ * hitender
+ * 5/24/20 3:16 AM
+ */
+public enum BusinessCustomerAttributeEnum {
+    AP("AP", "Approved"),
+    RJ("RJ", "Reject"),
+    GY("GY", "Grocery"),
+    LQ("LQ", "Liquor");
+
+    private final String description;
+    private final String name;
+
+    BusinessCustomerAttributeEnum(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
