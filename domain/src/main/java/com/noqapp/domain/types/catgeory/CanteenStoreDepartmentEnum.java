32c1ef73e7544095dcc56453aad3016c91c2a07e
hth <hth@users.noreply.github.com>
2020-05-25 05:09:32 -0700

    updated with customer attribute

--- domain/src/main/java/com/noqapp/domain/types/catgeory/CanteenStoreDepartmentEnum.java
+++ domain/src/main/java/com/noqapp/domain/types/catgeory/CanteenStoreDepartmentEnum.java
@@ -1,21 +1,31 @@
 package com.noqapp.domain.types.catgeory;
 
+import static com.noqapp.domain.types.BusinessCustomerAttributeEnum.GY;
+import static com.noqapp.domain.types.BusinessCustomerAttributeEnum.LQ;
+
+import com.noqapp.domain.types.BusinessCustomerAttributeEnum;
+
 /**
  * hitender
  * 5/23/20 3:27 PM
  */
 public enum CanteenStoreDepartmentEnum {
 
-    ACT("ACT", "Serving"),
-    RET("RET", "Ex-Servicemen"),
-    OFF("OFF", "Officers");
+    GA("GA", "Grocery Serving", GY),
+    GR("GR", "Grocery Ex-Servicemen", GY),
+    GO("GO", "Grocery Officers", GY),
+    LA("LA", "Liquor Serving", LQ),
+    LR("LR", "Liquor Ex-Servicemen", LQ),
+    LO("LO", "Liquor Officers", LQ);
 
     private final String description;
     private final String name;
+    private final BusinessCustomerAttributeEnum businessCustomerAttribute;
 
-    CanteenStoreDepartmentEnum(String name, String description) {
+    CanteenStoreDepartmentEnum(String name, String description, BusinessCustomerAttributeEnum businessCustomerAttribute) {
         this.name = name;
         this.description = description;
+        this.businessCustomerAttribute = businessCustomerAttribute;
     }
 
     public String getDescription() {
@@ -24,6 +34,10 @@ public enum CanteenStoreDepartmentEnum {
 
     public String getName() {
         return name;
+    }
+
+    public BusinessCustomerAttributeEnum getBusinessCustomerAttribute() {
+        return businessCustomerAttribute;
     }
 
     @Override
