68d215ba0eb729398b53f423de3e80e0db56815d
hth <hth@users.noreply.github.com>
2020-06-30 23:24:38 -0700

    log error

--- service/src/main/java/com/noqapp/service/FreemarkerService.java
+++ service/src/main/java/com/noqapp/service/FreemarkerService.java
@@ -2,6 +2,9 @@ package com.noqapp.service;
 
 import static org.springframework.ui.freemarker.FreeMarkerTemplateUtils.processTemplateIntoString;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
@@ -19,26 +22,37 @@ import java.util.Map;
  */
 @Service
 public class FreemarkerService {
+    private static final Logger LOG = LoggerFactory.getLogger(FreemarkerService.class);
 
     private FreeMarkerConfigurationFactory freemarkerConfiguration;
 
     @Autowired
     public FreemarkerService(
-            @SuppressWarnings ("SpringJavaAutowiringInspection")
-            FreeMarkerConfigurationFactory freemarkerConfiguration
+        @SuppressWarnings ("SpringJavaAutowiringInspection")
+        FreeMarkerConfigurationFactory freemarkerConfiguration
     ) {
         this.freemarkerConfiguration = freemarkerConfiguration;
     }
 
     String freemarkerToString(String ftl, Map<String, Object> rootMap) throws IOException, TemplateException {
-        Configuration cfg = freemarkerConfiguration.createConfiguration();
-        Template template = cfg.getTemplate(ftl);
-        return processTemplateIntoString(template, rootMap);
+        try {
+            Configuration cfg = freemarkerConfiguration.createConfiguration();
+            Template template = cfg.getTemplate(ftl);
+            return processTemplateIntoString(template, rootMap);
+        } catch (Exception e) {
+            LOG.error("Failed processing ftl={} {}", ftl, rootMap);
+            throw e;
+        }
     }
 
     String freemarkerToStringComplex(String ftl, Map<String, Map<String, Object>> rootMap) throws IOException, TemplateException {
-        Configuration cfg = freemarkerConfiguration.createConfiguration();
-        Template template = cfg.getTemplate(ftl);
-        return processTemplateIntoString(template, rootMap);
+        try {
+            Configuration cfg = freemarkerConfiguration.createConfiguration();
+            Template template = cfg.getTemplate(ftl);
+            return processTemplateIntoString(template, rootMap);
+        } catch (Exception e) {
+            LOG.error("Failed processing ftl={} {}", ftl, rootMap);
+            throw e;
+        }
     }
 }
