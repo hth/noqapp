c0d26951adf594efd14926fc7e2e5c5b978e48c1
hth <hth@users.noreply.github.com>
2020-10-07 22:56:04 -0700

    Added displayCase and product available date

--- domain/src/main/java/com/noqapp/domain/StoreProductEntity.java
+++ domain/src/main/java/com/noqapp/domain/StoreProductEntity.java
@@ -1,5 +1,6 @@
 package com.noqapp.domain;
 
+import com.noqapp.common.utils.DateUtil;
 import com.noqapp.domain.annotation.Mobile;
 import com.noqapp.domain.json.JsonStoreProduct;
 import com.noqapp.domain.types.ProductTypeEnum;
@@ -15,6 +16,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.util.Date;
 
 /**
  * hitender
@@ -76,6 +78,9 @@ public class StoreProductEntity extends BaseEntity {
     //TODO product description references to html location.
     @Field("PR")
     private String productReference;
+
+    @Field("AD")
+    private Date availableDate;
 
     @Field("DC")
     private boolean displayCaseTurnedOn;
@@ -203,6 +208,15 @@ public class StoreProductEntity extends BaseEntity {
 
     public StoreProductEntity setProductReference(String productReference) {
         this.productReference = productReference;
+        return this;
+    }
+
+    public Date getAvailableDate() {
+        return availableDate == null ? getUpdated() : availableDate;
+    }
+
+    public StoreProductEntity setAvailableDate(Date availableDate) {
+        this.availableDate = availableDate;
         return this;
     }
 
