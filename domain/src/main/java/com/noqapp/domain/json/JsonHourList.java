21532b9091eeedddaa3fc69c335a5e326ad78d4b
hth <hth@users.noreply.github.com>
2020-10-11 00:26:04 -0700

    Added jsonHourList

---
+++ domain/src/main/java/com/noqapp/domain/json/JsonHourList.java
@@ -0,0 +1,44 @@
+package com.noqapp.domain.json;
+
+import com.noqapp.common.utils.AbstractDomain;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * hitender
+ * 10/11/20 12:21 AM
+ */
+@SuppressWarnings({
+    "PMD.BeanMembersShouldSerialize",
+    "PMD.LocalVariableCouldBeFinal",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LongVariable",
+    "unused"
+})
+@JsonAutoDetect(
+    fieldVisibility = JsonAutoDetect.Visibility.ANY,
+    getterVisibility = JsonAutoDetect.Visibility.NONE,
+    setterVisibility = JsonAutoDetect.Visibility.NONE
+)
+@JsonPropertyOrder(alphabetic = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JsonHourList extends AbstractDomain {
+
+    @JsonProperty("hrs")
+    private List<JsonHour> jsonHours = new ArrayList<>();
+
+    public List<JsonHour> getJsonHours() {
+        return jsonHours;
+    }
+
+    public JsonHourList setJsonHours(List<JsonHour> jsonHours) {
+        this.jsonHours = jsonHours;
+        return this;
+    }
+}
