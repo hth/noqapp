a88506b880d302b6a06b6489897aea6159e62584
hth <hth@users.noreply.github.com>
2020-10-13 12:40:41 -0700

    Added friendly description

--- domain/src/main/java/com/noqapp/domain/types/QueueStatusEnum.java
+++ domain/src/main/java/com/noqapp/domain/types/QueueStatusEnum.java
@@ -5,28 +5,30 @@ package com.noqapp.domain.types;
  * Date: 3/2/17 11:28 AM
  */
 public enum QueueStatusEnum {
-    S("S", "Start"),
-    R("R", "Re-Start"),
-    N("N", "Next"),
+    S("S", "Start", ""),
+    R("R", "Re-Start", ""),
+    N("N", "Next", ""),
     /* Pause is a status for Server/Merchant, Pause applies to just Server/Merchant. Queue never pauses. */
-    P("P", "Pause"),
-    D("D", "Done"),
-    C("C", "Closed"),
+    P("P", "Pause", ""),
+    D("D", "Done", ""),
+    C("C", "Closed", ""),
 
     /* These should not be used for FCM messages. These needs to be fixed in a separate enum. */
     /* B is when user joins with a different time on mobile. */
-    B("B", "Before Opening Time"),
-    A("A", "After Closing Time"),
-    X("X", "Business service limitation imposed"),
-    T("T", "You have been served today"),
-    L("L", "Reached available token");
+    B("B", "Before Opening Time", "Token issued before store opening"),
+    A("A", "After Closing Time", "Token issued after store closing"),
+    X("X", "Business service limitation imposed" , "You have been serviced in past. Please wait until few days to issue tokens"),
+    T("T", "You have been served today", "You have been served today"),
+    L("L", "Reached available token", "Reached maximum number of token");
 
     private final String name;
     private final String description;
+    private final String friendlyDescription;
 
-    QueueStatusEnum(String name, String description) {
+    QueueStatusEnum(String name, String description, String friendlyDescription) {
         this.name = name;
         this.description = description;
+        this.friendlyDescription = friendlyDescription;
     }
 
     public String getName() {
@@ -35,6 +37,24 @@ public enum QueueStatusEnum {
 
     public String getDescription() {
         return description;
+    }
+
+    public String friendlyDescription() {
+        String reason;
+
+        switch (this) {
+            case B:
+            case A:
+            case X:
+            case L:
+                reason = this.friendlyDescription;
+                break;
+            default:
+                reason = "Due to some limitation token was not issued";
+                break;
+        }
+
+        return reason;
     }
 
     @Override
