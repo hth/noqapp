39d66a893b1cac51ae2db3e43c61eddc33416943
hth <hth@users.noreply.github.com>
2020-11-29 20:03:54 -0800

    Added display token

--- domain/src/main/java/com/noqapp/domain/json/JsonQueueChangeServiceTime.java
+++ domain/src/main/java/com/noqapp/domain/json/JsonQueueChangeServiceTime.java
@@ -31,6 +31,9 @@ public class JsonQueueChangeServiceTime extends AbstractDomain {
     @JsonProperty("t")
     private int token;
 
+    @JsonProperty("dt")
+    private String displayToken;
+
     @JsonProperty("osl")
     private String oldTimeSlotMessage;
 
@@ -46,6 +49,15 @@ public class JsonQueueChangeServiceTime extends AbstractDomain {
 
     public JsonQueueChangeServiceTime setToken(int token) {
         this.token = token;
+        return this;
+    }
+
+    public String getDisplayToken() {
+        return displayToken;
+    }
+
+    public JsonQueueChangeServiceTime setDisplayToken(String displayToken) {
+        this.displayToken = displayToken;
         return this;
     }
 
