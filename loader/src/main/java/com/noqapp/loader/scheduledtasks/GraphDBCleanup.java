929438b13cc06e8ff3ebf2f0fd3e2061c877f9ae
hth <hth@users.noreply.github.com>
2021-01-30 15:50:59 -0800

    Clean up

--- loader/src/main/java/com/noqapp/loader/scheduledtasks/GraphDBCleanup.java
+++ loader/src/main/java/com/noqapp/loader/scheduledtasks/GraphDBCleanup.java
@@ -53,7 +53,7 @@ public class GraphDBCleanup {
     public void cleanupSinceNotAccessed() {
         try {
             long countPerson = personN4jManager.deleteNotAccessedSince(DateUtil.minusDays(1));
-            long countBusinessCustomer = businessCustomerN4jManager.deleteNotAccessedSince(DateUtil.minusDays(6));
+            long countBusinessCustomer = businessCustomerN4jManager.deleteNotAccessedSince(DateUtil.minusDays(1));
             LOG.info("Deleted non accessed GraphDB {} {}", countBusinessCustomer, countPerson);
         } catch (Exception e) {
             LOG.error("Failed to delete reason={}", e.getLocalizedMessage(), e);
