e5895fde3f6e3dec667221ba75e0b94a2be9e9d0
hth <hth@users.noreply.github.com>
2021-01-30 17:03:06 -0800

    Removed relationship

--- domain/src/main/java/com/noqapp/domain/neo4j/AnomalyN4j.java
+++ domain/src/main/java/com/noqapp/domain/neo4j/AnomalyN4j.java
@@ -2,7 +2,6 @@ package com.noqapp.domain.neo4j;
 
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Relationship;
 
 /**
  * hitender
@@ -14,24 +13,12 @@ public class AnomalyN4j {
     @Id
     private String qid;
 
-    @Relationship(type = "CUSTOMER_ID", direction = Relationship.OUTGOING)
-    private BusinessCustomerN4j businessCustomerN4j;
-
     public String getQid() {
         return qid;
     }
 
     public AnomalyN4j setQid(String qid) {
         this.qid = qid;
-        return this;
-    }
-
-    public BusinessCustomerN4j getBusinessCustomerN4j() {
-        return businessCustomerN4j;
-    }
-
-    public AnomalyN4j setBusinessCustomerN4j(BusinessCustomerN4j businessCustomerN4j) {
-        this.businessCustomerN4j = businessCustomerN4j;
         return this;
     }
 }
