a123872a5396b67a5b628b18a864e4d2a46c7a4f
hth <hth@users.noreply.github.com>
2021-02-06 11:15:39 -0800

    Added unique constraint geoPoint  to location

--- loader/src/main/java/com/noqapp/loader/listener/LoaderInitializationBean.java
+++ loader/src/main/java/com/noqapp/loader/listener/LoaderInitializationBean.java
@@ -146,6 +146,13 @@ public class LoaderInitializationBean {
             } else {
                 LOG.info("Constraint found biz_name_unique_id");
             }
+
+            if (!constraintIds.contains("location_geo_point")) {
+                Result result = session.query("CREATE CONSTRAINT location_geo_point ON (l:Location) ASSERT l.geoPoint IS UNIQUE;", Collections.EMPTY_MAP);
+                LOG.info("Constraint on geoPoint in Location added={}", result.queryStatistics().containsUpdates());
+            } else {
+                LOG.info("Constraint found location_geo_point");
+            }
         } catch (ClientException ex) {
             LOG.error("Failed creating constraint reason={}", ex.getLocalizedMessage(), ex);
         }
