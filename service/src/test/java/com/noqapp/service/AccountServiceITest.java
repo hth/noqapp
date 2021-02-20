7c654b5cba0c1a13e89603b40e44366f081f991e
hth <hth@users.noreply.github.com>
2021-02-20 14:18:33 -0800

    Added test to validated account

---
+++ service/src/test/java/com/noqapp/service/AccountServiceITest.java
@@ -0,0 +1,35 @@
+package com.noqapp.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.noqapp.domain.EmailValidateEntity;
+import com.noqapp.domain.UserAccountEntity;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.IOException;
+
+/**
+ * hitender
+ * 2/20/21 2:07 PM
+ */
+@DisplayName("Account Service API")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Tag("api")
+class AccountServiceITest extends ITest {
+
+    @Test
+    void validateAccount() {
+        UserAccountEntity userAccount1 = accountService.findByUserId("rocketd@r.com");
+        assertEquals(false, userAccount1.isAccountValidated(), "Account is not validated");
+
+        EmailValidateEntity emailValidate1 = emailValidateService.saveAccountValidate(userAccount1.getQueueUserId(), userAccount1.getUserId());
+        accountService.validateAccount(emailValidate1, userAccount1.getQueueUserId());
+        UserAccountEntity userAccount1_after_validation = accountService.findByQueueUserId(userAccount1.getQueueUserId());
+        assertEquals(true, userAccount1_after_validation.isAccountValidated(), "Account is validated");
+    }
+}
