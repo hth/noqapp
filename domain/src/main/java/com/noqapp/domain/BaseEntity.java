2efcc850428337417565545cbd9c12d8c978579d
hth <hth@users.noreply.github.com>
2021-05-29 09:06:04 -0700

    Updated log

--- domain/src/main/java/com/noqapp/domain/BaseEntity.java
+++ domain/src/main/java/com/noqapp/domain/BaseEntity.java
@@ -73,7 +73,7 @@ public abstract class BaseEntity implements Serializable {
             try {
                 value = (String) annotation.annotationType().getMethod(attributeName).invoke(annotation);
             } catch (Exception annotationException) {
-                LOG.error("Annotation reading error={}", annotationException.getLocalizedMessage(), annotationException);
+                LOG.error("Annotation reading error=\"{}\"", annotationException.getLocalizedMessage(), annotationException);
             }
         }
 
